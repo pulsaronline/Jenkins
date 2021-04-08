8b3eee389eb5ae5f6861d2a45ead6497a5b8ca10
Pulsaronline <beslite@gmail.com>
2021-04-08 19:12:46 +0300

    add StudentRegistrationFormTests class

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -4,12 +4,14 @@ import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class StudentRegistrationFormTests {
     private final String BASE_URL = "https://demoqa.com/automation-practice-form";
+    String pageHeader = "Thanks for submitting the form";
 
     @Test
     void successfulFillFormTest(){
@@ -18,7 +20,7 @@ public class StudentRegistrationFormTests {
                 lastName = faker.name().lastName(),
                 email = faker.internet().emailAddress(),
                 gender = "Other",
-                mobile = faker.number().digits(10),
+                phoneNumber = faker.number().digits(10),
                 dayOfBirth = String.valueOf(faker.number().numberBetween(1, 28)),
                 monthOfBirth = "March",
                 yearOfBirth = String.valueOf(faker.number().numberBetween(1900, 2010)),
@@ -27,14 +29,70 @@ public class StudentRegistrationFormTests {
                 hobby1 = "Sports",
                 hobby2 = "Reading",
                 hobby3 = "Music",
-                picture = "1.png",
-                currentAddress = faker.address().fullAddress(),
+                filename = "1.png",
+                address = faker.address().fullAddress(),
                 state = "Uttar Pradesh",
                 city = "Merrut";
 
         step("Open students registration form", () -> {
             open(BASE_URL);
             $(".pactice-form-wrapper").shouldHave(text("Student Registration form"));
+        });
+        step("Fill students registration form", () -> {
+                    step("Fill common data", () -> {
+                    $("#firstName").setValue(firstName);
+                    $("#lastName").setValue(lastName);
+                    $("#userEmail").setValue(email);
+                    $(byText(gender)).click();
+                    $("#userNumber").setValue(phoneNumber);
+                    });
+                    step("Set date", () -> {
+                        $("#dateOfBirthInput").click();
+                        $(".react-datepicker__month-select").selectOptionByValue(monthOfBirth);
+                        $(".react-datepicker__year-select").selectOptionByValue(yearOfBirth);
+                        $(".react-datepicker__month").$(byText(dayOfBirth)).click();
+                    });
+                    step("Set subjects", () -> {
+                        $("#subjectsContainer").click();
+                        $("#subjectsInput").setValue(subject1).pressEnter();
+                        $("#subjectsInput").setValue(subject2).pressEnter();
+                    });
+                    step("Set hobbies", () -> {
+                        $(byText(hobby1)).click();
+                        $(byText(hobby2)).click();
+                        $(byText(hobby3)).click();
+                    });
+                    step("Upload file", () ->
+                        $("#uploadPicture").uploadFromClasspath("img/" + filename));
+                    step("Set address", () -> {
+                        $("#currentAddress").setValue(address).pressTab();
+                        $("#state").scrollTo().click();
+                        $(byText(state)).click();
+                        $("#city").click();
+                        $(byText(city)).click();
+                    });
+                    step("Submit the form", () -> $("#submit").pressEnter());
+                });
+        step("Validate data", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text(pageHeader));
+            $(".table-responsive").shouldHave(
+                    text(firstName),
+                    text(lastName),
+                    text(email),
+                    text(gender),
+                    text(phoneNumber),
+                    text(dayOfBirth),
+                    text(monthOfBirth),
+                    text(yearOfBirth),
+                    text(subject1),
+                    text(subject2),
+                    text(hobby1),
+                    text(hobby2),
+                    text(hobby3),
+                    text(filename),
+                    text(address),
+                    text(state),
+                    text(city));
         });
     }
 }
