b8bb5b51638f08c1c4626f20f2ccde727680dbbf
Pulsaronline <beslite@gmail.com>
2021-04-09 16:53:17 +0300

    capabilities.setCapability("browserName", "chrome");

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,6 +18,7 @@ public class TestBase {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("browserName", "chrome");
         capabilities.setCapability("browserVersion", "89.0");
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
