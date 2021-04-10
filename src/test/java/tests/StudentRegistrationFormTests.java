b9c747329d12c7a798431678b33b85d055267337
Pulsaronline <beslite@gmail.com>
2021-04-10 14:00:37 +0300

    code clean 51

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.StudentRegistrationFormPage;
-import tests.*;
+
 public class StudentRegistrationFormTests {
     StudentRegistrationFormPage formPage;
 
     @Test
     @DisplayName("Positive test")
-//  @Disabled
+  @Disabled
     public void okTest() {
        TestBase.beforeAll();
        formPage = new StudentRegistrationFormPage();
